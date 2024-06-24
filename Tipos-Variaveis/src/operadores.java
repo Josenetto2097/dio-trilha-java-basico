770e63dc1b7b24e7ae7aef95f1bd2e86e9ae182e
Jose Neto <jnetto2097@gmail.com>
2024-06-24 18:28:32 -0300

    Sistemas

---
+++ Tipos-Variaveis/src/operadores.java
@@ -0,0 +1,17 @@
+public class operadores {
+    public static void main(String[] args) {
+    boolean condicao1=true;
+
+    boolean condicao2=true;
+
+    if(condicao1 && condicao2){
+	    System.out.println("As duas condições são verdadeiras" );
+    }
+
+    if(condicao1 || condicao2){
+	    System.out.println("Um dos valores é verdadeiro" );
+    }
+        System.out.println("Fim");
+
+    }
+}
