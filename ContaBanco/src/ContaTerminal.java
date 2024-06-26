5cab1294e9b0b814b9c61d57bdd74bc6272a707e
Jose Neto <jnetto2097@gmail.com>
2024-06-26 19:31:05 -0300

    Update ContaTerminal.java
    Teste Final

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,36 @@
+import java.util.Scanner;
+
+
 public class ContaTerminal {
-    
+
+    //Atributos
+    private String nome;
+    private String numAgencia;
+    private int numConta;
+    private double saldo;
+
+    public ContaTerminal(String nome, String numAgencia, int numConta){
+        this.nome = nome;
+        this.numAgencia = numAgencia;
+        this.numConta = numConta;
+        this.saldo = 237.48;
+    }
+
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor digite o seu nome! ");
+            String nome = sc.nextLine();
+
+        System.out.println("Por favor digite o número de sua Agência! ");
+             String numAgencia = sc.nextLine();
+
+        System.out.println("Por favor digite o número de sua Conta! ");
+            int numConta = sc.nextInt();
+
+        ContaTerminal conta = new ContaTerminal(nome, numAgencia, numConta);
+
+        System.out.println("Olá " + conta.nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + conta.numAgencia + ", conta " + conta.numConta + " e seu saldo " + conta.saldo + " já está disponível para saque");
+
     }
 }
